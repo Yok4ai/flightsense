13b123a471cdb7dbd4a3a85604d1b17c4fa179d0
Yok4ai <148655260+Yok4ai@users.noreply.github.com>
2024-03-18 20:09:34 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flightsense
+ A travel companion app with a focus on fostering a social community
---
+++ android/app/src/main/kotlin/com/example/flightsense/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flightsense
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

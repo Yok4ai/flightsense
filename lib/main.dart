5569a7b788b7afe23f57c294cbe4b3ce80d2ec9b
shuvosharkr <shuvoo0505@gmail.com>
2024-03-27 21:41:10 +0600

    Added Navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: HomePage(),
+      home: splashscreen(),
     );
   }
 }

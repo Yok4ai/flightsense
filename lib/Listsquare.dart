281b8d76a3fb10230f1128a598f25e3fca6e801a
Yok4ai <yokai.re77@gmail.com>
2024-04-03 14:44:55 +0600

    Refactored Code

--- lib/Listsquare.dart
+++ lib/Listsquare.dart
@@ -3,20 +3,20 @@ import "package:flutter/material.dart";
 class Listsquare extends StatelessWidget {
   final String child;
 
-  Listsquare({required this.child});
+  const Listsquare({super.key, required this.child});
   @override
   Widget build(BuildContext context) {
     return Padding(
-            padding: const EdgeInsets.only(top: 12.0, right: 12, left: 12),
+            padding: const EdgeInsets.only(top: 40, right: 25, left: 25),
             child: Container(
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
               color: Colors.deepPurple,
-              borderRadius: const BorderRadius.all(Radius.circular(30)),
+              borderRadius: BorderRadius.all(Radius.circular(30)),
               ),
               height: 150,
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(child,style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),),
+                padding: const EdgeInsets.all(15.0),
+                child: Text(child,style: const TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),),
               ),
 
             ),
